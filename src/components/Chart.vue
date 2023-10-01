41ed2b55601824c9fa0ac13bc9900847ca291397
Ammad Aslam <ammada33@gmail.com>
2023-10-02 01:34:04 +0500

    Add product model created, Chart Component created

---
+++ src/components/Chart.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <canvas ref="chart" :width="width" :height="height"></canvas>
+    </div>
+</template>
+  
+<script>
+import Chart from 'chart.js';
+
+export default {
+    props: {
+        chartData: {
+            type: Object,
+            required: true,
+        },
+        width: {
+            type: Number,
+            default: 600,
+        },
+        height: {
+            type: Number,
+            default: 400,
+        },
+    },
+    mounted() {
+        const ctx = this.$refs.chart.getContext('2d');
+        new Chart(ctx, this.chartData);
+    },
+};
+</script>
+  
